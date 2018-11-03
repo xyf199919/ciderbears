3ef9bfa006f6b4acb9440d6a6dff0aeae8289b17
xyf199919 <40393702+xyf199919@users.noreply.github.com>
2018-11-03 00:08:50 -0700

    Add files via upload

---
+++ GroupDrive/New Folder With Items/GroupDrive/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  GroupDrive
+//
+//  Created by Yunfang Xiao on 11/2/18.
+//  Copyright © 2018 CiderBears. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    @IBOutlet weak var create: UIButton!
+    
+    
+}
+
